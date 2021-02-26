d4308d273251e43b6aa1877cceb9bf1a11cf81a2
Hidouche AIT YOUCEF <hidouche.aityoucef@gmail.com>
2021-02-26 11:30:12 +0100

    Allow player to enter name and store player's name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 console.log("projet chifoumi");
 
 alert(" Welcome to the game CHIFOUMI");
+
+var userName = prompt("Enter you name : ");
+alert("Tank you " + userName );
+alert("Are you ready " + userName);
