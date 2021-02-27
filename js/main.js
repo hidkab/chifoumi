0f65db83be40b266734a7b3127abaeddda6f0229
Hidouche AIT YOUCEF <hidouche.aityoucef@gmail.com>
2021-02-27 09:14:25 +0100

    Allow player to chose/playe

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ console.log("projet chifoumi");
 
 alert(" Welcome to the game CHIFOUMI");
 
-var userName = prompt("Enter you name : ");
+var userName = prompt("Enter you name : "); //affichage d'un champs de saisie 
 alert("Tank you " + userName );
 alert("Are you ready " + userName);
+
+var gammeElements = [ //déclaration d'un tableau
+    "pierre",
+    "feuille",
+    "ciseaux"
+];
+
+var player = window.prompt("Choose : " + gammeElements);
